7fed91245a86186762edafd16c6aead6d7aac3b2
Jose Alberto <josee-45@hotmail.com>
2017-10-31 12:32:38 -0600

    fix boopstrap popper

--- gulpfile.js
+++ gulpfile.js
@@ -34,10 +34,7 @@ gulp.task('images', function(){
 });
 
 gulp.task('js', function() {
-    return gulp.src(['src/app.js'])
-        .pipe(browserify({
-            insertGlobals : true
-        }))
+    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.js', 'node_modules/jquery/dist/jquery.min.js'])
         .pipe(gulp.dest("dist/js"))
         .pipe(browserSync.stream());
 });
